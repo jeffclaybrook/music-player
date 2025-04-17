94f39f26dd4b6fbf36f085e5d7df4a197c07006a
Jeff Claybrook <jeffclaybrook@MacBookPro-40.home>
2025-04-17 18:46:03 -0500

    Made PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import withPWA from "next-pwa"
+
+const isDev = process.env.NODE_ENV === "development"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: isDev
+})(nextConfig)
